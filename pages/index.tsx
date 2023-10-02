533486ac338ad2f9c1dac5a4376515d804e63be6
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-02 17:17:28 -0400

    post work

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "@/components/Header";
+import { sanityClient, urlFor } from "../sanity";
 
-const inter = Inter({ subsets: ["latin"] });
+interface Props {
+  posts: [Post];
+}
 
-export default function Home() {
+export default function Home(props: Props) {
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -38,3 +41,26 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+  _id,
+    title,
+    slug,
+    author -> {
+      name, 
+        image
+    },
+    description,
+    mainImage,
+    slug
+}`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
